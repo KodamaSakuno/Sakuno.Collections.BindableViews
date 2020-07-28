421e04a35532343cd7a380129f2fd101f1752a74
神樹桜乃 <kodama@sakuno.moe>
2020-07-28 22:29:09 +0800

    Update

--- tests/Sakuno.Collections.BindableViews.Tests/ConcatenatedCollectionViewTests.cs
+++ tests/Sakuno.Collections.BindableViews.Tests/ConcatenatedCollectionViewTests.cs
@@ -7,7 +7,7 @@ namespace Sakuno.Collections.BindableViews.Tests
     public class ConcatenatedCollectionViewTests
     {
         [Fact]
-        public static void SimpleCollections()
+        public void SimpleCollections()
         {
             var sources = new[]
             {
@@ -22,14 +22,13 @@ namespace Sakuno.Collections.BindableViews.Tests
         }
 
         [Fact]
-        public static void OuterObservableCollections_Add()
+        public void OuterObservableCollections_Add()
         {
             var source = new ObservableCollection<int[]>()
             {
                 new[] { 5 },
                 new[] { 2, 3 },
             };
-
             var concatenated = new ConcatenatedCollectionView<int>(source);
 
             Assert.Equal(3, concatenated.Count);
@@ -42,11 +41,10 @@ namespace Sakuno.Collections.BindableViews.Tests
         }
 
         [Fact]
-        public static void InnerObservableCollections_Add()
+        public void InnerObservableCollections_Add()
         {
             var a = new ObservableCollection<int>();
             var b = new ObservableCollection<int>();
-
             var concatenated = new ConcatenatedCollectionView<int>(new[] { a, b });
 
             Assert.Empty(concatenated);
@@ -60,11 +58,10 @@ namespace Sakuno.Collections.BindableViews.Tests
         }
 
         [Fact]
-        public static void BothObservableCollections_Add()
+        public void BothObservableCollections_Add()
         {
             var outer = new ObservableCollection<ObservableCollection<int>>();
             var inner = new ObservableCollection<int>();
-
             var concatenated = new ConcatenatedCollectionView<int>(outer);
 
             Assert.Empty(concatenated);
@@ -81,7 +78,7 @@ namespace Sakuno.Collections.BindableViews.Tests
         }
 
         [Fact]
-        public static void OuterObservableCollections_Remove()
+        public void OuterObservableCollections_Remove()
         {
             var source = new ObservableCollection<int[]>()
             {
@@ -89,7 +86,6 @@ namespace Sakuno.Collections.BindableViews.Tests
                 new[] { 4, 5, 6 },
                 new[] { 3, 2 },
             };
-
             var concatenated = new ConcatenatedCollectionView<int>(source);
 
             Assert.Equal(6, concatenated.Count);
@@ -102,12 +98,11 @@ namespace Sakuno.Collections.BindableViews.Tests
         }
 
         [Fact]
-        public static void InnerObservableCollections_Remove()
+        public void InnerObservableCollections_Remove()
         {
             var a = new ObservableCollection<int>() { 1, 3, 2 };
             var b = new ObservableCollection<int>() { 4 };
             var c = new ObservableCollection<int>() { 9, 8 };
-
             var concatenated = new ConcatenatedCollectionView<int>(new[] { a, b, c });
 
             Assert.Equal(6, concatenated.Count);
@@ -121,11 +116,10 @@ namespace Sakuno.Collections.BindableViews.Tests
         }
 
         [Fact]
-        public static void BothObservableCollections_Remove()
+        public void BothObservableCollections_Remove()
         {
             var inner = new ObservableCollection<int>() { 1, 5, 2, 4 };
             var outer = new ObservableCollection<ObservableCollection<int>>() { inner };
-
             var concatenated = new ConcatenatedCollectionView<int>(outer);
 
             Assert.Equal(new[] { 1, 5, 2, 4 }, concatenated);
@@ -140,7 +134,7 @@ namespace Sakuno.Collections.BindableViews.Tests
         }
 
         [Fact]
-        public static void OuterObservableCollections_Replace()
+        public void OuterObservableCollections_Replace()
         {
             var source = new ObservableCollection<int[]>()
             {
@@ -148,7 +142,6 @@ namespace Sakuno.Collections.BindableViews.Tests
                 new[] { 1 },
                 new[] { 4, 5, 6 },
             };
-
             var concatenated = new ConcatenatedCollectionView<int>(source);
 
             Assert.Equal(6, concatenated.Count);
@@ -161,12 +154,11 @@ namespace Sakuno.Collections.BindableViews.Tests
         }
 
         [Fact]
-        public static void InnerObservableCollections_Replace()
+        public void InnerObservableCollections_Replace()
         {
             var a = new ObservableCollection<int>() { 1, 3, 2 };
             var b = new ObservableCollection<int>() { 4, 5, 6 };
             var c = new ObservableCollection<int>() { 7, 8 };
-
             var concatenated = new ConcatenatedCollectionView<int>(new[] { a, b, c });
 
             Assert.Equal(8, concatenated.Count);
@@ -179,14 +171,13 @@ namespace Sakuno.Collections.BindableViews.Tests
         }
 
         [Fact]
-        public static void OuterObservableCollections_Clear()
+        public void OuterObservableCollections_Clear()
         {
             var source = new ObservableCollection<int[]>()
             {
                 new[] { 1 },
                 new[] { 2, 3 },
             };
-
             var concatenated = new ConcatenatedCollectionView<int>(source);
 
             Assert.Equal(3, concatenated.Count);
@@ -198,12 +189,11 @@ namespace Sakuno.Collections.BindableViews.Tests
         }
 
         [Fact]
-        public static void InnerObservableCollections_Clear()
+        public void InnerObservableCollections_Clear()
         {
             var a = new ObservableCollection<int>() { 1, 2, 3 };
             var b = new ObservableCollection<int>() { 4, 5, 6 };
             var c = new ObservableCollection<int>() { 7, 8 };
-
             var concatenated = new ConcatenatedCollectionView<int>(new[] { a, b, c });
 
             Assert.Equal(8, concatenated.Count);
